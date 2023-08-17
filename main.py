966f0f9ce71a60b1afb9cb00f4202ca75b7e5bee
Enigma1769 <enigma1769@proton.me>
2023-08-17 22:23:14 +0530

    Fetch Query

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import mysql.connector as msc
-from  password import pwd
-mydb = msc.connector.connect(
+import mypass
+pwd= mypass.pwd()
+mydb = msc.connect(
   host="localhost",
   user="root",
   password=pwd,
   database="dhaage"
 )
 
-print(mydb)
+#print(mydb)
+
+# Create a cursor
+mycursor = mydb.cursor()
+
+# Execute the query
+query = 'SELECT * FROM clothes_info'
+mycursor.execute(query)
+
+# Fetch and print the results
+result = mycursor.fetchall()
+for row in result:
+    print(row)
+
+# Close the cursor and database connection
+mycursor.close()
+mydb.close()
