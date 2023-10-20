3926be28568d57ee61d102758ca6a9e44d48ad54
Enigma1769 <enigma1769@proton.me>
2023-10-20 22:07:50 +0530

    Fixed no purchase_history.csv error

--- history.py
+++ history.py
@@ -1,9 +1,13 @@
 import csv
 
 def view_purchase_history():
-    with open('purchase_history.csv', 'r') as file:
-        reader = csv.reader(file)
-        print("History Content Of the File")
-        for row in reader:
-            return ('Item ID: ', row[0], ', Quantity: ', row[1], ', Date: ', row[2])
+    try: 
+        with open('purchase_history.csv', 'r') as file:
+            reader = csv.reader(file)
+            print("History Content Of the File")
+            for row in reader:
+                return ('Item ID: ', row[0], ', Quantity: ', row[1], ', Date: ', row[2])
+    except:
+        print("No Items in history")
+    
 view_purchase_history()
