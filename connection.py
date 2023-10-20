08b561e8c8450cc593eebb2e50ee5e66e3c293a2
Enigma1769 <enigma1769@proton.me>
2023-10-20 23:18:55 +0530

    Store password if not found

--- connection.py
+++ connection.py
@@ -2,6 +2,17 @@ import mysql.connector as msc
 import mypass
 pwd= mypass.pwd()
 def connect():
+    try:
+        with open('password.txt', 'r+') as passfile:
+            pwd = passfile.read()
+            if not pwd:
+                pwd = input("Enter the MySQL DB password: ")
+                passfile.seek(0)
+                passfile.write(pwd)
+    except FileNotFoundError:
+        pwd = input("Enter the MySQL DB password: ")
+        with open('password.txt', 'w') as passfile:
+            passfile.write(pwd)
     mydb = msc.connect(
         host="localhost",
         user="root",
