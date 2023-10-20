a1fd380435a165d4cb2fe4223fba6f748fef9e9d
Enigma1769 <enigma1769@proton.me>
2023-10-20 23:05:05 +0530

    Updated history.py for more verbosing

--- history.py
+++ history.py
@@ -6,8 +6,8 @@ def view_purchase_history():
             reader = csv.reader(file)
             print("History Content Of the File")
             for row in reader:
-                print('Item Name: ', row[0], ', Quantity: ', row[1], ', Date: ', row[2])
+                print('Item ID: ', row[0], ', Brand Name: ', row[1], ', Cloth Type: ', row[2], ', Quantity: ', row[3], ', Total Price: ', row[4], ', Date: ', row[5])
     except:
         print("No Items in history")
-    
+        
 view_purchase_history()
